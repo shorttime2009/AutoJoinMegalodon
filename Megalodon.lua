1f58b9ab30fa5d0cdd3d7c44135d92a689fdc36d
shorttime2009 <134736650+shorttime2009@users.noreply.github.com>
2025-10-26 22:23:42 +0100

    Update kick message in Megalodon.lua

--- Megalodon.lua
+++ Megalodon.lua
@@ -1,12 +1,2 @@
---[[
-     Version 1.2, made by frel0/xamel
---]]
-
-local Message = loadstring(game:HttpGet("https://raw.githubusercontent.com/frel0/FakeKickLibrary/main/FakeKickLibrary.lua", true))()
-
-Message:Crash("An unexpected error occured and Roblox needs to quit. Were sorry!")
-wait(3)
-Message:Kick("You have been kicked from this experience: Banned for 5456 days because youre so so gay")
-wait(3)
-Message:RickRoll() 
--- DOESNT WORK ON SYNAPSE X!
+Message:Kick("You have been kicked from this experience: Banned for 5456 days because youre so so gay victor quit sucking dicks be normal just like galaad")
+wait(10)
